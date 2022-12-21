0e544f74c54aff153d0e8aa1151ba7647247a1dd
Евгений Ефимченко <evgeniy@efimchenko.ru>
2022-12-21 21:43:28 +0300

    Update v1.1.3

--- app/addons/ee_change_myasniki/func.php
+++ app/addons/ee_change_myasniki/func.php
@@ -18,3 +18,26 @@ function fn_ee_change_myasniki_products_sorting(&$sorting, $simple_mode) {
 function fn_ee_change_myasniki_get_products_pre(&$params, $items_per_page, $lang_code) {
 	$params['match'] = 'all';
 }
+
+function fn_ee_change_myasniki_update_cart_products_post(&$cart, $product_data, $auth) {	
+	if (isset($_REQUEST['dispatch']) && $_REQUEST['dispatch'] == 'order_management.update_totals' && $_REQUEST['result_ids'] == 'om_ajax_*') {
+		foreach ($product_data as $k => $item) {
+			$cart['products'][$k]['discount'] = 0;
+			$cart['products'][$k]['display_price'] = (float)$item['base_price'];
+			$cart['products'][$k]['discount_prc'] = 0;
+			$cart['product_groups'][0]['products'][$k]['discount'] = 0;
+			$cart['product_groups'][0]['products'][$k]['discount_prc'] = 0;
+			$cart['product_groups'][0]['products'][$k]['display_price'] = $cart['products'][$k]['base_price'];
+		}
+		$cart['discount'] = 0;
+	}
+}
+
+function fn_ee_change_myasniki_calculate_cart_items($cart, &$cart_products, $auth, $apply_cart_promotions) {
+	foreach ($cart_products as $k => $v) {
+		unset($cart_products[$k]['discount']);
+		unset($cart_products[$k]['discount_prc']);
+		$cart_products[$k]['price'] = $cart_products[$k]['original_price'];
+		$cart_products[$k]['out_of_stock_actions'] = 'N';
+	}
+}
--- app/addons/ee_change_myasniki/init.php
+++ app/addons/ee_change_myasniki/init.php
@@ -4,5 +4,7 @@ if (!defined('BOOTSTRAP')) { die('Access denied'); }
 
 fn_register_hooks (
 	'products_sorting',
-	'get_products_pre'
+	'get_products_pre',
+	'update_cart_products_post',
+	'calculate_cart_items'
 );
