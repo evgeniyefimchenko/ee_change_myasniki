777368b6d0e475cd47686dbb9c70d6cb160990be
Евгений Ефимченко <evgeniy@efimchenko.ru>
2023-10-25 13:40:42 +0300

    fix bug

--- app/addons/ee_change_myasniki/func.php
+++ app/addons/ee_change_myasniki/func.php
@@ -2,18 +2,18 @@
 if (!defined('BOOTSTRAP')) { die('Access denied'); }
 
 use Tygh\Registry;
-use Tygh\Debugger;
 
+// Not used
 function fn_ee_change_myasniki_install() {
 	return true;
 }
-
+// Not used
 function fn_ee_change_myasniki_uninstall() {
 	return true;
 }
 
 function fn_ee_change_myasniki_products_sorting(&$sorting, $simple_mode) {	
-	// $sorting['code'] = array('description' => 'code', 'default_order' => 'desc');
+
 }
 
 function fn_ee_change_myasniki_get_products_pre(&$params, $items_per_page, $lang_code) {
@@ -21,19 +21,16 @@ function fn_ee_change_myasniki_get_products_pre(&$params, $items_per_page, $lang
 }
 
 function fn_ee_change_myasniki_update_cart_products_pre(&$cart, &$product_data, $auth) {	
-	// fn_print_die($product_data);
+
 }
 
 function fn_ee_change_myasniki_update_cart_products_post(&$cart, &$product_data, $auth) {
-	// $cart['products'] = $product_data;
-	// fn_print_die($cart);
-	// fn_print_die($product_data); Тут скидка ещё есть! Посмотреть в foreach ($group['chosen_shippings'] as $shipping_key => $shipping) {
+
 }
 
 function fn_ee_change_myasniki_calculate_cart_items($cart, &$cart_products, $auth, $apply_cart_promotions) {
 	
 }
-
 
 /**
 * Сортируем массив по ключу product_code
